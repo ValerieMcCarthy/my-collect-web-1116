8c912f25f98151234c1560eca346a31a95889a6a
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-08 13:04:10 -0500

    Yielded to collect. Fixed error in spec file.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect (array)
+	new_array = []
+	i = 0
+	while i < array.length
+	new_array << yield(array[i])
+	i = i + 1
+	end
+	new_array
+end
 
+#array_names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array_names) do |name|
+#	name.split.first
+#end
+
+
+#collection = ['ruby', 'javascript', 'python', 'objective-c']
+#my_collect(collection) do |lang|
+ # lang.upcase
+#end
 
